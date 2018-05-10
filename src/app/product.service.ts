056182a002d417a6e7c1562326938a56624119c6
microsby0 <microsby0@gmail.com>
2018-05-10 10:57:21 -0400

    adjust for crappy formatting

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  private albumUrl = '../assets/album.json'
-  constructor(private http: Http) { }
+  private _albumUrl = '../assets/album.json'
+  constructor(private _http: Http) { }
 
   private getAlbum(id: number) {
-    return this.http.get(this.albumUrl).map((response => response.json()));
+    return this._http.get(this._albumUrl).map((response => response.json()));
   }
 }
