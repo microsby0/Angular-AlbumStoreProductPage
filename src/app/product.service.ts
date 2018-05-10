e8d60e5f84dada0ecfdf6fce914ec0b8f84d8d44
microsby0 <microsby0@gmail.com>
2018-05-10 10:54:07 -0400

    add product service

---
+++ src/app/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+  private albumUrl = '../assets/album.json'
+  constructor(private http: Http) { }
+
+  private getAlbum(id: number) {
+    return this.http.get(this.albumUrl).map((response => response.json()));
+  }
+}
